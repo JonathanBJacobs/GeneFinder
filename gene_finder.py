2c0e4cf91c46a3c72099d6d3f25d0db81fb30e5d
Jonathan <Jonathan Jacobs>
2016-01-31 13:53:19 -0500

    week 1 completed push

--- gene_finder.py
+++ gene_finder.py
@@ -2,7 +2,7 @@
 """
 YOUR HEADER COMMENT HERE
 
-@author: YOUR NAME HERE
+@author: Jonathan Jacobs
 
 """
 
@@ -25,13 +25,33 @@ def get_complement(nucleotide):
 
         nucleotide: a nucleotide (A, C, G, or T) represented as a string
         returns: the complementary nucleotide
+
+        The two added unit tests test the other two possible inputs that the
+        function will handle. That means every viable input is tested
+
+        The last unit test makes sure that the funciton doesnt have a fit if it
+        gets a character that it shouldn't
     >>> get_complement('A')
     'T'
     >>> get_complement('C')
     'G'
+    >>> get_complement('G')
+    'C'
+    >>> get_complement('T')
+    'A'
+    >>> get_complement('B')
+
     """
-    # TODO: implement this
-    pass
+    if nucleotide == 'A':
+        return 'T'
+    elif nucleotide == 'T':
+        return 'A'
+    elif nucleotide == 'G':
+        return 'C'
+    elif nucleotide == 'C':
+        return 'G'
+    else:
+        return None
 
 
 def get_reverse_complement(dna):
@@ -40,13 +60,18 @@ def get_reverse_complement(dna):
 
         dna: a DNA sequence represented as a string
         returns: the reverse complementary DNA sequence represented as a string
+
+        I don't believe any other unit tests are necessary because the two
+        following unit tests test all four nucleotides within the sequences
     >>> get_reverse_complement("ATGCCCGCTTT")
     'AAAGCGGGCAT'
     >>> get_reverse_complement("CCGCGTTCA")
     'TGAACGCGG'
     """
-    # TODO: implement this
-    pass
+    complementDna = ''
+    for index in range(len(dna)):
+        complementDna = get_complement(dna[index]) + complementDna
+    return complementDna
 
 
 def rest_of_ORF(dna):
@@ -57,13 +82,24 @@ def rest_of_ORF(dna):
 
         dna: a DNA sequence
         returns: the open reading frame represented as a string
+
+        The added unit test is to test to make sure that the entire string is
+        returned if there is no end condon
+    >>> rest_of_ORF("ATGTGCCC")
+    'ATGTGCCC'
     >>> rest_of_ORF("ATGTGAA")
     'ATG'
     >>> rest_of_ORF("ATGAGATAGG")
     'ATGAGA'
     """
-    # TODO: implement this
-    pass
+    sequence = 'ATG'
+    index = 3
+    while index < len(dna)-2:
+        if dna[index:index+3] == 'TAG' or dna[index:index+3] == 'TAA' or dna[index:index+3] == 'TGA':
+            return sequence
+        sequence += dna[index:index+3]
+        index += 3
+    return dna
 
 
 def find_all_ORFs_oneframe(dna):
@@ -76,11 +112,25 @@ def find_all_ORFs_oneframe(dna):
 
         dna: a DNA sequence
         returns: a list of non-nested ORFs
+
+    the added test just provides anohter option to make sure that the function
+    doesn't just work on this one specific example
     >>> find_all_ORFs_oneframe("ATGCATGAATGTAGATAGATGTGCCC")
     ['ATGCATGAATGTAGA', 'ATGTGCCC']
+
+    >>> find_all_ORFs_oneframe("ATGCCCCCCCCCTGACGGATGCCCCGG")
+    ['ATGCCCCCCCCC', 'ATGCCCCGG']
     """
-    # TODO: implement this
-    pass
+    skipUntil = 0
+    sequences = []
+    temp = ''
+    for index in range(len(dna)):
+        if index < len(dna) and index >= skipUntil and index >= 3 and index % 3 == 0:
+            if dna[index-3:index] == 'ATG':
+                temp = rest_of_ORF(dna[index-3:])
+                sequences.append(temp)
+                skipUntil = index + len(temp)
+    return sequences
 
 
 def find_all_ORFs(dna):
@@ -93,11 +143,22 @@ def find_all_ORFs(dna):
         dna: a DNA sequence
         returns: a list of non-nested ORFs
 
+        the extra test was to make sure that the function works with more than
+        just a single input
     >>> find_all_ORFs("ATGCATGAATGTAG")
     ['ATGCATGAATGTAG', 'ATGAATGTAG', 'ATG']
+
+    >>> find_all_ORFs("CATGCCATGCTGA")
+    ['ATGCTGA', 'ATGCCATGC']
+
     """
-    # TODO: implement this
-    pass
+    temp = []
+    orfs = []
+    for index in range(3):
+        temp = find_all_ORFs_oneframe(dna[index:])
+        for orf in temp:
+            orfs.append(orf)
+    return orfs
 
 
 def find_all_ORFs_both_strands(dna):
@@ -109,8 +170,9 @@ def find_all_ORFs_both_strands(dna):
     >>> find_all_ORFs_both_strands("ATGCGAATGTAGCATCAAA")
     ['ATGCGAATG', 'ATGCTACATTCGCAT']
     """
-    # TODO: implement this
-    pass
+    strand1 = find_all_ORFs(dna)
+    strand2 = find_all_ORFs(get_reverse_complement(dna))
+    return strand1 + strand2
 
 
 def longest_ORF(dna):
@@ -160,6 +222,7 @@ def gene_finder(dna):
     """
     # TODO: implement this
     pass
+
 
 if __name__ == "__main__":
     import doctest
